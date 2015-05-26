75d825209678635832e49396a3659a95a02c489c
Summon Yang <SummonYang1991@gmail.com>
2015-05-26 11:51:26 +0800

    rename apue.c -> apue_err.c

--- apue.h
+++ apue.h
@@ -1,9 +1,16 @@
 #ifndef _APUE_H
 #define _APUE_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define MAXLINE     4096
 
 
 
 
+void err_sys(const char *, ...) __attribute__((noreturn));
 
 #endif
