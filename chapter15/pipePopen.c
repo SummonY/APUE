e384c2d1ebe043f079bc6dea1677e81b7ecbe5cd
Summon Yang <SummonYang1991@gmail.com>
2015-05-26 20:11:39 +0800

    popen pclose implement pipe gager.

---
+++ chapter15/pipePopen.c
@@ -0,0 +1,34 @@
+#include "apue.h"
+
+#define PAGER       "${PAGER:-more}"
+
+int main(int argc, const char *argv[])
+{
+    char line[MAXLINE];
+    FILE *fpin, *fpout;
+
+    if (argc != 2) {
+        err_quit("usage: exec <pathname>");
+    }
+    if ((fpin = fopen(argv[1], "r")) == NULL) {
+        err_sys("Can't open %s\n", argv[1]);
+    }
+    if ((fpout = popen(PAGER, "w")) == NULL) {
+        err_sys("popen error");
+    }
+
+    // copy argv[1] to pager
+    while (fgets(line, MAXLINE, fpin) != NULL) {
+        if (fputs(line, fpout) == EOF) {
+            err_sys("fputs error to pipe");
+        }
+    }
+
+    if (ferror(fpin)) {
+        err_sys("fgets error");
+    }
+    if (pclose(fpout) == -1) {
+        err_sys("pclose error");
+    }
+    exit(0);
+}
