3107ba7f48158fe5317d88aa8adddd1eea6a4033
Summon Yang <SummonYang1991@gmail.com>
2015-08-16 13:16:54 +0800

    add pthread_mutex_timedlock DEMO.

--- apue.h
+++ apue.h
@@ -1,11 +1,17 @@
 #ifndef _APUE_H
 #define _APUE_H
 
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/termios.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <signal.h>
+
+#include <time.h>
 
 #define MAXLINE     4096
 
